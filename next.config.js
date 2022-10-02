2b55735aee10e256352023f6c9237601347c3120
NightlyHerb <82109216+nightlyherb@users.noreply.github.com>
2022-10-02 11:15:51 +0900

    Edit: Migrate to basic scaling system

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
